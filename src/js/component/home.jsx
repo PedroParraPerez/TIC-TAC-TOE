4b2b8986944e96b7bfe7358c639b967b075a4b3b
PedroParraPerez <pedroparra06091994@gmail.com>
2022-02-02 14:48:58 +0000

    update tiktak

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,25 +1,124 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useState, useEffect } from "react";
+import "../../styles/index.css";
 
 //create your first component
 const Home = () => {
+	const [table, setTable] = useState([
+		[undefined, undefined, undefined],
+		[undefined, undefined, undefined],
+		[undefined, undefined, undefined],
+	]);
+	const [turn, setTurn] = useState("x");
+
+	// const CheckMayorDiagonal = () => {
+	// 	let i = 1;
+	// 	let ctrl = false;
+
+	// 	while (ctrl && i < table.length) {
+	// 		if (table[i][i] != table[i - 1][i - 1]) {
+	// 			ctrl = true;
+	// 		} else {
+	// 			i += 1;
+	// 		}
+	// 	}
+	// 	return ctrl;
+	// };
+
+	const CheckRow = () => {
+		let i = 0;
+		let ctrl = true;
+
+		while (ctrl && i < table.length) {
+			console.log(`i value: ${i}`);
+			// recorrer columnas => buscar NO coincidencias
+			let j = 1;
+			let ctrl2 = true;
+			while (ctrl2 && j < table[i].length) {
+				if (
+					table[i][j] !== table[i][j - 1] ||
+					typeof table[i][j] === "undefined"
+				) {
+					ctrl2 = false;
+				} else {
+					j += 1;
+				}
+			}
+
+			ctrl = !ctrl2;
+			i += 1;
+		}
+
+		return !ctrl;
+	};
+	const CheckColumnns = () => {
+		let j = 0;
+		let ctrl = true;
+
+		while (ctrl && j < table.length) {
+			console.log(`j value: ${j}`);
+			// recorrer columnas => buscar NO coincidencias
+			let i = 1;
+			let ctrl2 = true;
+			while (ctrl2 && i < table[j].length) {
+				if (
+					table[i][j] !== table[i - 1][j] ||
+					typeof table[i][j] === "undefined"
+				) {
+					ctrl2 = false;
+				} else {
+					i += 1;
+				}
+			}
+
+			ctrl = !ctrl2;
+			j += 1;
+		}
+
+		return !ctrl;
+	};
+
+	const CheckWinner = () => {
+		if (CheckRow() || CheckColumnns()) {
+			alert("Hay un ganador");
+			window.location.reload(true);
+		}
+	};
+	const ChangeTurn = () => setTurn(turn === "x" ? "o" : "x");
+
+	const SetValue = (i, j) => {
+		if (typeof table[i][j] === "undefined") {
+			if (!CheckWinner()) {
+				const tmp = table;
+				tmp[i][j] = turn;
+				setTable(table);
+				ChangeTurn();
+			}
+		} else {
+			alert("Hey esta posicion ya estaba ocupada");
+		}
+	};
+	if (CheckWinner()) {
+		const tmp = table;
+		tmp[i][j] = turn;
+		setTable(table);
+		ChangeTurn();
+	}
 	return (
-		<div>
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+		<>
+			<table>
+				<tbody>
+					{table.map((row, i) => (
+						<tr key={i}>
+							{row.map((column, j) => (
+								<td onClick={() => SetValue(i, j)} key={j}>
+									<div className="item">{column}</div>
+								</td>
+							))}
+						</tr>
+					))}
+				</tbody>
+			</table>
+		</>
 	);
 };
 
